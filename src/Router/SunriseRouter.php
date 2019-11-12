5ad21e23f3ed7df470e96f2777e48fc6d7e86601
Dominik Zogg <dominik.zogg@gmail.com>
2019-11-12 19:01:31 +0100

    sunrise-router

---
+++ src/Router/SunriseRouter.php
@@ -0,0 +1,130 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chubbyphp\Framework\Router;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Sunrise\Http\Router\Exception\MethodNotAllowedException;
+use Sunrise\Http\Router\Exception\RouteNotFoundException;
+use Sunrise\Http\Router\Router;
+
+final class SunriseRouter implements RouterInterface
+{
+    /**
+     * @var array<RouteInterface>
+     */
+    private $routes = [];
+
+    /**
+     * @var Router
+     */
+    private $router;
+
+    /**
+     * @param array<RouteInterface> $routes
+     */
+    public function __construct(array $routes)
+    {
+        $this->routes = $this->getRoutesByName($routes);
+        $this->router = $this->createRouter($routes);
+    }
+
+    public function match(ServerRequestInterface $request): RouteInterface
+    {
+        try {
+            $sunriseRoute = $this->router->match($request);
+
+            /** @var RouteInterface $route */
+            $route = $this->routes[$sunriseRoute->getName()];
+
+            return $route->withAttributes($sunriseRoute->getAttributes());
+        } catch (RouteNotFoundException $exception) {
+            throw RouterException::createForNotFound($request->getRequestTarget());
+        } catch (MethodNotAllowedException $exception) {
+            throw RouterException::createForMethodNotAllowed(
+                $request->getMethod(),
+                $exception->getAllowedMethods(),
+                $request->getRequestTarget()
+            );
+        }
+    }
+
+    /**
+     * @param array<string, string> $attributes
+     * @param array<string, mixed>  $queryParams
+     *
+     * @throws RouterException
+     */
+    public function generateUrl(
+        ServerRequestInterface $request,
+        string $name,
+        array $attributes = [],
+        array $queryParams = []
+    ): string {
+        $uri = $request->getUri();
+        $requestTarget = $this->generatePath($name, $attributes, $queryParams);
+
+        return $uri->getScheme().'://'.$uri->getAuthority().$requestTarget;
+    }
+
+    /**
+     * @param array<string, string> $attributes
+     * @param array<string, mixed>  $queryParams
+     *
+     * @throws RouterException
+     */
+    public function generatePath(string $name, array $attributes = [], array $queryParams = []): string
+    {
+        $path = $this->router->getRoute($name)->buildPath($attributes, false);
+
+        if ([] === $queryParams) {
+            return $this->basePath.$path;
+        }
+
+        return $this->basePath.$path.'?'.http_build_query($queryParams);
+    }
+
+    /**
+     * @param array<RouteInterface> $routes
+     *
+     * @return array<RouteInterface>
+     */
+    private function getRoutesByName(array $routes): array
+    {
+        $routesByName = [];
+        foreach ($routes as $route) {
+            $routesByName[$route->getName()] = $route;
+        }
+
+        return $routesByName;
+    }
+
+    /**
+     * @param array<RouteInterface> $routes
+     */
+    private function createRouter(array $routes): Router
+    {
+        $router = new Router();
+
+        foreach ($routes as $route) {
+            $router->route(
+                $route->getName(),
+                $route->getPath(),
+                [$route->getMethod()],
+                new class() implements RequestHandlerInterface {
+                    public function handle(ServerRequestInterface $request): ResponseInterface
+                    {
+                        throw new \LogicException('You should never get to this point');
+                    }
+                },
+                [],
+                $route->getAttributes()
+            );
+        }
+
+        return $router;
+    }
+}
