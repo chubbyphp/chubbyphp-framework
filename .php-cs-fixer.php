5fd44d7576b74921f863c4a6eebc8566ef89d8bb
Dominik Zogg <dominik.zogg@hotmail.com>
2022-10-01 10:15:54 +0200

    Drop php-cs-fixer config overrides

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -12,12 +12,6 @@ $finder = PhpCsFixer\Finder::create()
 /** @var array $config */
 $config = require __DIR__ . '/vendor/chubbyphp/chubbyphp-dev-helper/phpcs.php';
 
-unset($config['rules']['final_class']);
-
-$config['rules']['native_function_invocation'] = false;
-$config['rules']['phpdoc_to_param_type'] = ['scalar_types' => false];
-$config['rules']['phpdoc_to_return_type'] = false;
-
 return (new PhpCsFixer\Config)
     ->setIndent($config['indent'])
     ->setLineEnding($config['lineEnding'])
